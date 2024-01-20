53ddd7bb6aadd25cbbe6bb24f2bb1b18faaaed91
Tomoki Aonuma <uasi@uasi.jp>
2024-01-21 07:55:35 +0900

    Add /login

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,8 +3,8 @@ import { JSX } from "preact";
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      {...props}
       type="button"
+      {...props}
       class="text-2xl text-white active:text-black bg-black hover:bg-gray-600 active:bg-gray-300 rounded-md px-8 transition-colors"
     >
     </button>
---
+++ routes/login.tsx
@@ -0,0 +1,35 @@
+import { Handlers, PageProps } from "$fresh/server.ts"
+import { Button } from "../components/Button.tsx";
+
+interface Data {
+  error: string | undefined;
+}
+
+export const handler: Handlers<Data> = {
+  POST: async (req, ctx) => {
+    const formData = await req.formData();
+    const password = formData.get("password");
+
+    if (password === Deno.env.get("ADMIN_PASSWORD")) {
+      return ctx.render({ error: "TODO: Log in" });
+    } else {
+      return ctx.render({ error: "Invalid password" });
+    }
+  },
+};
+
+export default function Home({ data }: PageProps<Data | undefined>) {
+  const { error } = data ?? {};
+
+  return (
+    <div class="p-8 flex flex-col gap-4">
+      <form action="/login" method="post">
+        <div class="flex flex-col items-start gap-4">
+          <input type="password" name="password" class="text-2xl border-2 rounded-md" />
+          <Button type="submit">Log in</Button>
+        </div>
+      </form>
+      {error ? <p class="px-1 text-red-500">{error}</p> : null}
+    </div>
+  );
+}
